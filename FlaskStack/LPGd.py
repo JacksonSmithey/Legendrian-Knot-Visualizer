4524e7daf7a263361f0792d7a8a83c45ceb246b3
jss243 <jss243@txstate.edu>
2025-10-30 13:14:15 -0500

    Support for user determined constants

--- FlaskStack/LPGd.py
+++ FlaskStack/LPGd.py
@@ -7,49 +7,82 @@ Created on Wed Oct 23 18:59:36 2024
 import numpy as np
 import sympy as sp
 import pandas as pd
+from itertools import combinations
+import html
+import re
 
 def LPG(form):
+
+    def Validate(form):
+        for input in form:
+            input = html.escape(input)
+            input = re.sub(r'[^a-zA-Z0-9+\-*\/\(\)\s]', '', input)
+        if int(form["eDim"][0]) not in range(1, 6):
+            raise ValueError("E-Dimension must be in the range [1,5]")
+        if len(form["aList"])//3 not in range(0, 21):
+            raise ValueError("A-Variable count may not exceed 20")
+        if len(form["genFam"]) not in range(0, 800):
+            raise ValueError("Genfam contains too many characters. To bypass, edit LPGd.py Validation().")
+        if float(form["granularity"][0]) < .0005:
+            raise ValueError("Granularity has a minimum value of .0005")
+    try:
+        Validate(form)
+    except ValueError as e:
+        print(f"Error: {e}")
+        return
     
     #DEMO FORM INPUT
-    #form = {"eDim":1, "aDim":0, "genFam":"", "pThree":False, "pTop":False, "pFront":False, "granularity":1}
-    #form["eDim"] = 2
-    #form["aDim"] = 0
+    #form["eDim"] = 1
+    #form["aList"] = {0, 1, 1, 1, 1.5, 0.5} #{a1_start, a1_stop, a1_increment, a2_start, a2_stop, a2_increment}       
     #form["genFam"] = "(4-x**2)*e1-e1**3"
-    #Trefoil
-    #form["genFam"] = "(1/5)*e2**5+(1/3)*((e1-0)**2+(((x**2+4)*(x**2-16))/16))*e2**3+((e1-((1/16)*(x+3)*x*(x-3)))**2-0.5)*e2+((e1+((1/16)*(x+3)*x*(x-3)))**2-0.5)"
     #form["pThree"] = True
     #form["pTop"] = True
     #form["pFront"] = True
     #form["granularity"] = .1
     print(form)
 
-    #User input (up to 5 of each)
+    #Rename commonly used vars
     dimE = int(form["eDim"][0])
-    dimA = int(form["aDim"][0])
-
-    aiList = []
+    dimA = int(len(form["aList"]) // 3)
+    genfam = str(form["genFam"][0])
+    aList = form["aList"]
+    print('dimA', dimA)
     
     #Constants
     RES = float(form["granularity"][0])
     ZOOM_ADDEND = 5
     VIEW_TEST_CODE = 1
+
+    #Generate combinations of a_i values
+    ai_array = []
+    for i in range(0, dimA):
+        ai_vals = []
+        ai_start = float(form["aList"][3*i])
+        ai_stop = float(form["aList"][3*i + 1])
+        ai_increment = float(form["aList"][3*i + 2])
+        while ai_start <= ai_stop:
+            ai_vals.append(ai_start)
+            ai_start += ai_increment
+        ai_array.append(ai_vals)
+    for ai_row in ai_array:
+        ai_vals.append(combinations(ai_array))
+    for p in ai_vals:
+        print(p)
+
     
-    #Declare symbolic variables
+    #Declare symbolic variables and functions
     e1, e2, e3, e4, e5 = sp.symbols('e1 e2 e3 e4 e5', real = True)
-    E = [e1, e2, e3, e4, e5]
-    E = E[:dimE]
+    E = [e1, e2, e3, e4, e5] ; E = E[:dimE]
     a1, a2, a3, a4, a5 = sp.symbols('a1 a2 a3 a4 a5', real = True)
-    A = [a1, a2, a3, a4, a5]
-    A = A[:dimA] 
-    genfam = form["genFam"][0]
-    
-    #Declare symbolic functions
+    A = [a1, a2, a3, a4, a5] ; A = A[:dimA] 
+
     x = sp.Symbol('x', real=True)
     f = sp.Function('f')(x, tuple(E))
     f = sp.parse_expr(genfam, local_dict={ 'x':x
-                                                ,'e1':e1,'e2':e2,'e3':e3,'e4':e4,'e5':e5
-                                                ,'a1':a1,'a2':a2,'a3':a3,'a4':a4,'a5':a5})    
-    f = sp.simplify(f)   
+                                        ,'e1':e1,'e2':e2,'e3':e3,'e4':e4,'e5':e5
+                                        ,'a1':a1,'a2':a2,'a3':a3,'a4':a4,'a5':a5})
+    
+    f = sp.simplify(f)
     
     #Calculate dfe
     DFE = []
@@ -59,102 +92,94 @@ def LPG(form):
     #Calculate dfx
     dfx = sp.diff(f,x)
 
-    #Solve for gradient(f) = 0
+    #Solve grad(f)=0 to find bounds for x
     GRAD_F = [dfx]
     for i in range(dimE):
         GRAD_F.append(DFE[i])
     match dimE:
-        case 1:
-            zerograd = sp.solve(GRAD_F, x, e1)
-        case 2:
-            zerograd = sp.solve(GRAD_F, x, e1, e2)
-        case 3:
-            zerograd = sp.solve(GRAD_F, x, e1, e2, e3)
-        case 4:
-            zerograd = sp.solve(GRAD_F, x, e1, e2, e3, e4)
-        case 5:
-            zerograd = sp.solve(GRAD_F, x, e1, e2, e3, e4, e5)
+        case 1: zerograd = sp.solve(GRAD_F, x, e1)
+        case 2: zerograd = sp.solve(GRAD_F, x, e1, e2)
+        case 3: zerograd = sp.solve(GRAD_F, x, e1, e2, e3)
+        case 4: zerograd = sp.solve(GRAD_F, x, e1, e2, e3, e4)
+        case 5: zerograd = sp.solve(GRAD_F, x, e1, e2, e3, e4, e5)
 
-    #Find min and max of 0 gradient points
-    xmin = float('inf')
-    xmax = float('-inf')
-    for point in zerograd:        
-        if isinstance(point[0], sp.Float) or isinstance(point[0], sp.Integer):
-            if point[0] < xmin:
-                xmin = point[0]
-            if point[0] > xmax:
-                xmax = point[0] 
-    xmax = xmax + ZOOM_ADDEND
-    xmin = xmin - ZOOM_ADDEND
+    #Loop over all combinations of a_i values
+    for k in range(ai_vals.shape[0]):
+        print('k:', k)
+        ais = ai_vals
     
-    #Array to store solutions
-    solution_points = []
+        #Find min and max of 0 gradient points
+        xmin = float('inf')
+        xmax = float('-inf')
+        for point in zerograd:        
+            if isinstance(point[0], sp.Float) or isinstance(point[0], sp.Integer):
+                if point[0] < xmin:
+                    xmin = point[0]
+                if point[0] > xmax:
+                    xmax = point[0] 
+        xmax = xmax + ZOOM_ADDEND
+        xmin = xmin - ZOOM_ADDEND
+        
+        #Array to store solutions
+        solution_points = []
+        #init lists for point coords
+        x_list = []
+        y_list = []
+        z_list = []
 
-    #init lists for point coords
-    x_list = []
-    y_list = []
-    z_list = []
+        #Loop from min x to max x
+        for i in np.arange(xmin, xmax, RES):
+            NEW_DFE = []
+            for eqn in DFE:
+                NEW_DFE.append(sp.simplify(sp.Subs(eqn, x, i)))
 
-    #Loop from min x to max x
-    for i in np.arange(xmin, xmax, RES):
-        NEW_DFE = []
-        for eqn in DFE:
-            NEW_DFE.append(sp.simplify(sp.Subs(eqn, x, i)))
+            #solve dfe = 0 given x
+            E.insert(0, i) #nonlinsolve requires list input
+            solution = sp.nonlinsolve(NEW_DFE, E)
+            E.pop(0)
 
-        #solve dfe = 0 given x
-        E.insert(0, i) #nonlinsolve requires list input
-        solution = sp.nonlinsolve(NEW_DFE, E)
-        E.pop(0)
-
-        for value in solution:                
-            #Catch and skip imaginary solutions
-            flag = False
-            for toople in value:
-                if('I' in str(toople)):
-                    flag = True
+            for value in solution:                
+                #Catch and skip imaginary solutions
+                flag = False
+                for toople in value:
+                    if('I' in str(toople)):
+                        flag = True
+                        break
+                if flag:
                     break
-            if flag:
-                break
-
-            #Assign a_i Values       
-            ais = {"ai1":1, "ai2":1, "ai3":1, "ai4":1, "ai5":1}
-            for i in range(1, dimA):
-                ais[f"ai{i}"] = aiList[i]
+                    
+                #Assign e_i values and substitute a_i values
+                xi = value[0]
+                eis = {"e1":1, "e2":1, "e3":1, "e4":1, "e5":1}
+                for i in range(1, dimE):
+                    eis[f"e{i}"] = value[i]
                 
-            #Assign e_i values and substitute a_i values
-            xi = value[0]
-            eis = {"ei1":1, "ei2":1, "ei3":1, "ei4":1, "ei5":1}
-            for i in range(1, dimE):
-                eis[f"ei{i}"] = value[i]
-                eis[f"ei{i}"] = eis[f"ei{i}"].subs(ais)
-
-            
-            #substitute e_i and a_i values into dfx and f(x, e_i) for y,z coords    
-            dfxi = dfx.subs({x:xi}, eis, ais)
-            y = dfxi.evalf()
-            fi = f.subs({x:xi}, eis, ais)
-            z = fi.evalf()
-            x_list.append(str(xi))
-            y_list.append(str(y))
-            z_list.append(str(z))
-            solution_points.append(solution)
-                
-            #Console Test Output
-            if VIEW_TEST_CODE == 1:
-                print('solution: ', solution)
-                print('Value: ', value, type(value))
-                print('xi: ', xi, type(xi))
-                print('ais: ', ais, type(ais))
-                print('eis: ', eis, type(eis))
-                print('dfxi: ', dfxi, type(dfxi))
-                print('y: ', y, type(y))
-                print('fi: ', fi, type(fi))
-                print('z', z, type(z))
+                #substitute e_i and a_i values into dfx and f(x, e_i) for y,z coords    
+                dfxi = dfx.subs({x:xi, **eis, **ais})
+                y = dfxi.evalf()
+                fi = f.subs({x:xi, **eis, **ais})
+                z = fi.evalf()
+                x_list.append(str(xi))
+                y_list.append(str(y))
+                z_list.append(str(z))
+                solution_points.append(solution)
+                    
+                #Console Test Output
+                if VIEW_TEST_CODE == 1:
+                    print('solution: ', solution)
+                    print('Value: ', value, type(value))
+                    print('a start,stop,increment:', aList[i]["start"])
+                    print('xi: ', xi, type(xi))
+                    print('eis: ', eis, type(eis))
+                    print('dfxi: ', dfxi, type(dfxi))
+                    print('y: ', y, type(y))
+                    print('fi: ', fi, type(fi))
+                    print('z', z, type(z))
     if VIEW_TEST_CODE == 1:
         print('solution points:', solution_points, type(solution_points))
         print('num e:', dimE)
         print('num a:', dimA)
-        print('aiList:', aiList, type(aiList))
+        print('aList:', aList, type(aList))
         print('E: ', E, type(E))
         print('A: ', A, type(A))
         print('f:', f, type(f))
---
+++ README.md
@@ -0,0 +1,43 @@
+About
+*****
+The LKV takes generating families for Legendrian knots and plots their embeddings in R^3.
+A generating family f_x for a Legendrian knot is a family of functions whose critical values encode a projection of the knot. 
+It turns out there's a diffeomorphism between the critical locus of f and the embedding 
+        {(x,y,z) | \frac{\partial(f)}{\partial(e)} = y and f(x,e)=z }
+
+Enter the generating family as a polynomial function f(x,E). Where E is an n-dimensional vector space.
+To vary a constant and create multiple graphs of the same generating family, use A={a1,a2,...}
+values and the provided table entry. 
+ex. with one E-dimension, and one a control value. Results in an unknot for a_1 > 0. 
+f(x,E) = (a_1-x^2)e_1-e_1^3
+
+Text Entry Rules:
+Polynomials must be written using Python math operators. 
+Meaning ^ must be replaced with ** for exponentiation (standard +, -, *, /) and careful use of parenthesis 
+to preserve order of operations. Especially among dividends and divisors. The sqrt() function is not yet 
+supported so use **(1/2) instead.
+When typing your generating family, use e1,e2,... for E variables and a1,a2,... for user determined constants. 
+
+Other Notes:
+The LKV will attempt to solve and plot anything, even if it's not Legendrian. 
+Exponential functions are supported in solving, but trig functions may not have all solutions returned.
+Multiple sets of graphs (for multiple a-values) will multiply calculation times.
+If a plot has no points or the page never loads, 
+check if the critical locus of f is empty or the solution set is infinite
+
+
+VSCode Startup Instructions for Running on Localhost
+****************************************************
+```
+pip install flask
+cd ./FlaskStack
+python -m flask --version
+set FLASK_APP=flaskApp.py
+set FLASK_DEBUG=1  //This automatically updates the webpage when source code is changed
+python -m flask run
+```
+**if these commands raise errors in the VSCode powershell terminal, 
+run them in a cmd prompt terminal instead and double check the filepath '/FlaskStack/'**
+
+Default development browser URL: http://127.0.0.1:5000
+
